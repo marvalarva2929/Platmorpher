09fef4e36d63f7d55e052e11b3ae814e376f7d39
marvalarva2929 <joshvigel329@outlook.com>
2023-05-18 23:12:29 -0500

    initial commit

---
+++ src/main.cpp
@@ -0,0 +1,9 @@
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_image.h>
+#include <iostream>
+#define nl '\n'
+using namespace std;
+
+int main(int argv, char* args[]) {
+		cout << "Hello World" << nl;
+}
